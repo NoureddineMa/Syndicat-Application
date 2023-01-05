1d63e0d9d902202b60d4334e2d741f409eeec0f6
NoureddineMa <noureddinemaher13@gmail.com>
2023-01-05 11:24:22 +0100

    set Middlewares in Clients Requests

--- server/Routers/ClientRouter.js
+++ server/Routers/ClientRouter.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 const {CreateClient , UpdateClient , DeleteClient , GetAllClient} = require('../Controllers/ClientController')
-// const syndicatMiddleware = require('../Middlewares/SyndicatMiddelwares')
-// const verifyToken = require('../Middlewares/verifyToken')
+const verifyToken = require('../Middlewares/verifyToken')
+const syndicatMiddleware = require('../Middlewares/SyndicatMiddelwares')
 
 
-app.post('/client' ,CreateClient)
-app.put('/client/:id' , UpdateClient)
-app.delete('/client/:id' , DeleteClient)
-app.get('/client' , GetAllClient)
+app.post('/client' ,verifyToken,syndicatMiddleware,CreateClient)
+app.put('/client/:id' ,verifyToken,syndicatMiddleware,UpdateClient)
+app.delete('/client/:id' ,verifyToken,syndicatMiddleware,DeleteClient)
+app.get('/client' ,verifyToken,syndicatMiddleware,GetAllClient)
 
 module.exports = app;
