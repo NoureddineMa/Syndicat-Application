2bfee12ed08b45260f676a96672974845eedac09
NoureddineMa <noureddinemaher13@gmail.com>
2023-01-05 11:20:44 +0100

    set Middlewares in Appartement Requests

--- server/Routers/AppartementRouters.js
+++ server/Routers/AppartementRouters.js
@@ -4,9 +4,12 @@ const app = express();
 const {CreateAppartement , DeleteAppartement , UpdateAppartement , GetAllAppartement} = require('../Controllers/AppartementController')
 const verifyToken = require('../Middlewares/verifyToken')
 const syndicatMiddleware = require('../Middlewares/SyndicatMiddelwares')
+
+
+
 app.post('/appartement' ,verifyToken,syndicatMiddleware,CreateAppartement)
-app.delete('/appartement/:id' , DeleteAppartement)
-app.put('/appartement/:id' , UpdateAppartement)
-app.get('/appartements' , GetAllAppartement)
+app.delete('/appartement/:id' ,verifyToken,syndicatMiddleware,DeleteAppartement)
+app.put('/appartement/:id' ,verifyToken,syndicatMiddleware,UpdateAppartement)
+app.get('/appartements' ,verifyToken,syndicatMiddleware,GetAllAppartement)
 
 module.exports = app;
