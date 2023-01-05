3f2512c040db5a733f8f98378d3557869d179b85
NoureddineMa <noureddinemaher13@gmail.com>
2023-01-05 11:30:08 +0100

    set Middlewares in Paiments  Requests

--- server/Routers/PaimentRouter.js
+++ server/Routers/PaimentRouter.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const {CreatePaiment , DeletePaiment , GetAllPaiments} = require("../Controllers/PaimentController");
+const verifyToken = require('../Middlewares/verifyToken')
+const SyndicatMiddelwares = require('../Middlewares/SyndicatMiddelwares')
 
-router.post('/paiment' ,CreatePaiment)
-router.delete('/paiment/:id' , DeletePaiment)
-router.get('/paiments' , GetAllPaiments)
+
+router.post('/paiment' ,verifyToken,SyndicatMiddelwares,CreatePaiment)
+router.delete('/paiment/:id' ,verifyToken,SyndicatMiddelwares,DeletePaiment)
+router.get('/paiments' ,verifyToken,SyndicatMiddelwares,GetAllPaiments)
 
 
 
