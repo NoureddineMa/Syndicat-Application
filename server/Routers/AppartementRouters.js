f9cdc0a97d0c5015eb13292290098e9a0c0a8563
NoureddineMa <noureddinemaher13@gmail.com>
2023-01-11 17:46:13 +0100

    fix function getSIngleAppartement in Router appartement

--- server/Routers/AppartementRouters.js
+++ server/Routers/AppartementRouters.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const {CreateAppartement , DeleteAppartement , UpdateAppartement , GetAllAppartement} = require('../Controllers/AppartementController')
+const {CreateAppartement , DeleteAppartement , UpdateAppartement , GetAllAppartement , GetSingleAppartement} = require('../Controllers/AppartementController')
 const verifyToken = require('../Middlewares/verifyToken')
 const syndicatMiddleware = require('../Middlewares/SyndicatMiddelwares')
 
@@ -11,6 +11,6 @@ app.post('/appartement' ,verifyToken,syndicatMiddleware,CreateAppartement)
 app.delete('/appartement/:id' ,verifyToken,syndicatMiddleware,DeleteAppartement)
 app.put('/appartement/:id' ,verifyToken,syndicatMiddleware,UpdateAppartement)
 app.get('/appartements' ,verifyToken,syndicatMiddleware,GetAllAppartement)
-app.get('/appartement/:id' ,verifyToken,syndicatMiddleware,GetAllAppartement)
+app.get('/appartement/:id' ,verifyToken,syndicatMiddleware,GetSingleAppartement)
 
 module.exports = app;
