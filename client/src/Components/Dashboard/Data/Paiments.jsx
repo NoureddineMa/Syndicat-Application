2bb2956d31e3c499f5ed7ac809e61fa5d40e3caa
NoureddineMa <noureddinemaher13@gmail.com>
2023-01-13 15:32:46 +0100

    delete paiment

--- client/src/Components/Dashboard/Data/Paiments.jsx
+++ client/src/Components/Dashboard/Data/Paiments.jsx
@@ -23,6 +23,21 @@ function Paiments() {
     }
    } ,[])
 
+   
+   // function to remove paiments : 
+   const removePaiment = (id) => {
+    axios.delete(`http://localhost:3001/api/admin/paiment/${id}`, {
+        headers: {Authorization: `Bearer ${token}`}
+    }).then((res) => {
+        let result = data.filter((paiment) => paiment._id !== id)
+        setData(result)
+        console.log(res.data.message);
+    }).catch((err) => {
+        console.log(err);
+    })
+   }
+
+
   return (
     <div>
         <div className='flex  justify-between '>
@@ -68,7 +83,7 @@ function Paiments() {
                     </td>
                     <td className="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-gray-200">
                         
-                                <button className="group relative inline-block text-sm font-medium text-[#DC0000] focus:outline-none focus:ring active:text-[#DC0000]">
+                                <button onClick={() => {removePaiment(paiment._id)}} className="group relative inline-block text-sm font-medium text-[#DC0000] focus:outline-none focus:ring active:text-[#DC0000]">
                                     <span className="absolute inset-0 translate-x-0.5 translate-y-0.5 bg-[#DC0000] transition-transform group-hover:translate-y-0 group-hover:translate-x-0" />
                                     <span className="relative block border border-current bg-white px-8 py-3">
                                         Delete
