308316941775b6e3bc4dac6221e266e7743df0fb
NoureddineMa <noureddinemaher13@gmail.com>
2023-01-17 11:18:51 +0100

    test AUTH

--- server/testing/Auth.test.js
+++ server/testing/Auth.test.js
@@ -51,4 +51,86 @@ describe('Auth', () => {
     })
 })
 
+
+
+})
+
+
+describe("POST /register"  , () => {
+
+    describe('Auth', () => {
+        it('should respond with a 400 status code', async () => {
+            const res = await request(app)
+            .post('/api/admin/register')
+            .send({
+                First_Name: '',
+                Second_Name: '',
+                Email: '',
+                Password: '',
+                Phone_Number: ''
+            })
+            expect(res.status).toBe(400)
+        })
+    })
+
+    describe('Auth', () => {
+        it('should respond with a 400 status code', async () => {
+            const res = await request(app)
+            .post('/api/admin/register')
+            .send({
+                First_Name: 'Maher',
+                Second_Name: 'noureddine',
+                Email: 'test@gmail.com',
+                Password: 'test',
+                Phone_Number: ''
+            })
+            expect(res.status).toBe(400)
+        })
+    })
+
+    describe('Auth', () => {
+        it('should respond with a 400 status code', async () => {
+            const res = await request(app)
+            .post('/api/admin/register')
+            .send({
+                First_Name: 'maher',
+                Second_Name: 'Noureddine',
+                Email: 'test@gmail.com',
+                Password: '',
+                Phone_Number: '0623329476'
+            })
+            expect(res.status).toBe(400)
+        })
+    })
+
+    describe('Auth', () => {
+        it('should respond with a 400 status code', async () => {
+            const res = await request(app)
+            .post('/api/admin/register')
+            .send({
+                First_Name: 'maher',
+                Second_Name: 'Noureddine',
+                Email: '',
+                Password: 'test',
+                Phone_Number: '0623329476'
+            })
+            expect(res.status).toBe(400)
+        })
+    })
+
+    describe('Auth', () => {
+        it('should respond with a 400 status code', async () => {
+            const res = await request(app)
+            .post('/api/admin/register')
+            .send({
+                First_Name: '',
+                Second_Name: 'Noureddine',
+                Email: 'test@gmail.com',
+                Password: 'test',
+                Phone_Number: '0623329476'
+            })
+            expect(res.status).toBe(400)
+        })
+    })
+
 })
